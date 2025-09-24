ddff1912fbe2de65ce4bf4904ddaa901bee68df8
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-09-24 14:51:23 +0700

    Changed vitest.config.ts.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vitest/config';
-import path from 'path';
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@mock-entry': path.resolve(__dirname, 'playgrounds/vue/dist/server/entry-server.js'),
-        },
-    },
     test: {
         testTimeout: 20000,
         sequence: {
